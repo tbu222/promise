bc4f47a59ee0961a29d4172edf518a5edfc91d5e
Thinh <thinhbui2911@gmai.com>
2023-04-13 16:14:49 -0700

    first commit

---
+++ part-1/app.js
@@ -0,0 +1,22 @@
+let favNumber = 6;
+let baseURL = "http://numbersapi.com";
+
+// 1.
+$.getJSON(`${baseURL}/${favNumber}?json`).then(data => {
+  console.log(data);
+});
+
+// 2.
+let favNumbers = [4, 2, 1];
+$.getJSON(`${baseURL}/${favNumbers}?json`).then(data => {
+  console.log(data);
+});
+
+// 3.
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${baseURL}/${favNumber}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("body").append(`<p>${data.text}</p>`));
+});
